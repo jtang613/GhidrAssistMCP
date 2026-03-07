ced94c3439507c853c9ba93bea957c5bcead290b
Jason Tang <jason.tang@badc0ded.net>
2026-03-07 04:21:05 -0500

    Update to support GhidrAssistMCP headless.

---
+++ ghidra_scripts/GAMCPStartServerScript.java
@@ -0,0 +1,4 @@
+// Ghidra script wrapper - delegates to compiled extension class.
+// Must NOT have a package declaration for Ghidra's script engine to load it.
+public class GAMCPStartServerScript extends ghidrassistmcp.scripts.GAMCPStartServerScript {
+}
